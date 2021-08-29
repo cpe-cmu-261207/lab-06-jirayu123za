03b2dde38f5f0b57722eedeb6f868060dabe6b6c
jirayu123za <76035470+jirayu123za@users.noreply.github.com>
2021-08-29 21:21:30 +0700

    ComponentsUpdated

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,11 @@
+const AboutMe = () =>{
+    return(
+        <div className = 'my-5'>
+            <div className = 'text-center space-y-3'>
+                <p className = 'text-2xl font-semibold'>About Me</p>
+                <p className = 'text-xl'>Jirayu Jitprem 630612096</p>
+            </div>
+        </div>
+    )
+}
+export default AboutMe
